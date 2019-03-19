49dd4797608622414ede031291f31b524498c145
Michiel Wouters <mwouters@W3008>
2019-03-19 10:53:45 +0100

    Added Repo & Images

---
+++ GarageManager/Repositories/ApplicationDbContext.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace GarageManager.Repositories
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext()
+            :base("name = GarageDB")
+        {
+
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            SeedData(modelBuilder);
+        }
+
+        private void SeedData(DbModelBuilder modelBuilder)
+        {
+            throw new NotImplementedException();
+        }
+
+        public virtual DbSet<Cart> Carts { get; set; }
+        public virtual DbSet<Product> Products { get; set; }
+        public virtual DbSet<ProductType> ProductTypes { get; set; }
+        public virtual DbSet<UserDetail> UserDetails { get; set; }
+    }
+}
