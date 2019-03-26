79f79106c19898f33b6121ea483f4d8a04a5ec5f
Michiel Wouters <mwouters@W3008>
2019-03-26 10:19:20 +0100

    Added Shopping cart and success page

--- GarageManager/Pages/Success.aspx.cs
+++ GarageManager/Pages/Success.aspx.cs
@@ -1,9 +1,8 @@
-﻿using System;
+﻿using GarageManager.Models;
+using GarageManager.Repositories;
+using Microsoft.AspNet.Identity;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.UI;
-using System.Web.UI.WebControls;
 
 namespace GarageManager.Pages
 {
@@ -11,7 +10,12 @@ namespace GarageManager.Pages
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            List<CartModel> carts = (List<CartModel>)Session[User.Identity.GetUserId()];
 
+            var repo = new CartRepo();
+            repo.MarkOrdersAsPaid(carts);
+
+            Session[User.Identity.GetUserId()] = null;
         }
     }
 }
